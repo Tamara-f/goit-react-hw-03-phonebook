3a7730c5e36537ea2cbdf0b020c76c97e71d8f1e
tamara-f <filatovatam@ukr.net>
2020-09-14 16:26:31 +0300

    Updates

---
+++ precache-manifest.9efd5b53f03eae06cad51043d9f97206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dc927c56521f7d30e4311cd348c92a",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "166070068394629d6947",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.3ac9b42c.chunk.css"
+  },
+  {
+    "revision": "91d476ab09bcb44b9619",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.4ac697a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.4ac697a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166070068394629d6947",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.98f338b5.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
